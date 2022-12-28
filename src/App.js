d0de0622818ecddd9af10aa7bb69a64765e201d3
lawrenceuchenye <lawuche29@gmail.com>
2022-12-28 19:01:39 +0100

    done i.guess

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
-import { useSelector } from "react-redux"
+import { useSelector,useDispatch } from "react-redux"
 
 function App() {
 	const counter=useSelector(state=>state.counter);
-	console.log(counter);
+	const dispatch=useDispatch();
+
+	const Increment=()=>{
+		dispatch({ type:"INC" });
+	}
+
+	const Decrement=()=>{
+		dispatch({ type:"DEC" });
+	}
 
 	return (
 	     <div>
 	         <h1>Counter App</h1>
 		     <h3>counter : { counter }</h3>
+		     <div>
+		          <button onClick={Increment}>
+		             Increase
+		            </button>
+		           <button onClick={Decrement}>
+		             Decrease
+		             </button>
+		       </div>
 	      </div>
   );
 }
--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,14 @@
 import { createStore } from "redux"
 
 const reducersFn=(state={ counter:10 },action)=>{
+	if( action.type == "INC" ){
+		return { counter :state.counter+1 }
+	}
+	
+	if( action.type == "DEC" ){
+		return { counter:state.counter-1 }
+	}
+
 	return state;
 }
 
